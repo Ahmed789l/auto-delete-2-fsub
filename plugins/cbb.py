d5ddd116bd818f6b568abac62a0d74865d0c7567
I'm Sanji <filesharingbot78@gmail.com>
2024-06-29 11:39:25 +0530

    Add files via upload

---
+++ plugins/cbb.py
@@ -0,0 +1,38 @@
+#(©)Codexbotz
+
+from pyrogram import __version__
+from bot import Bot
+from config import OWNER_ID
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text = f"<b>○ 𝐃ᴇᴠᴇʟᴏᴘᴇᴅ 𝐁ʏ : <a href='https://t.me/Straw_Hat_Bots'>𝐒ᴛʀᴀᴡ 𝐇ᴀᴛ ꭙ 𝐁ᴏᴛs</a>\n○ 𝐁ᴏᴛ 𝐔ᴘᴅᴀᴛᴇs : <a href='https://t.me/Straw_Hat_Bots'>𝐂ʟɪᴄᴋ 𝐇ᴇʀᴇ</a>\n○ 𝐁ᴏᴛ 𝐒ᴜᴘᴘᴏʀᴛ : <a href='https://t.me/Straw_hat_support'>𝐂ʟɪᴄᴋ 𝐇ᴇʀᴇ</a>\n○ 𝐀ɴɪᴍᴇ 𝐂ʜᴀᴛ : <a href='https://t.me/+BSL2tOp6BGE2MGU1'>𝐒ᴛʀᴀᴡ 𝐇ᴀᴛ ꭙ 𝐆ʀᴏᴜᴘ</a></b>",
+            disable_web_page_preview = True,
+            reply_markup = InlineKeyboardMarkup(
+                [
+                    [
+                    InlineKeyboardButton("⚡️ Cʟᴏsᴇ", callback_data = "close"),
+                    InlineKeyboardButton('🍁 Dᴇᴠᴇʟᴏᴘᴇʀs', url='https://t.me/Straw_Hat_Bots')
+                    ]
+                ]
+            )
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
+
+#⋗  ᴛᴇʟᴇɢʀᴀᴍ - @Codeflix_bots
+
+
+#- ᴄʀᴇᴅɪᴛ - Github - @Codeflix-bots , @erotixe
+#- ᴘʟᴇᴀsᴇ ᴅᴏɴ'ᴛ ʀᴇᴍᴏᴠᴇ ᴄʀᴇᴅɪᴛ..
+#- ᴛʜᴀɴᴋ ʏᴏᴜ ᴄᴏᴅᴇғʟɪx ʙᴏᴛs ғᴏʀ ʜᴇʟᴘɪɴɢ ᴜs ɪɴ ᴛʜɪs ᴊᴏᴜʀɴᴇʏ 
+#- ᴛʜᴀɴᴋ ʏᴏᴜ ғᴏʀ ɢɪᴠɪɴɢ ᴍᴇ ᴄʀᴇᴅɪᴛ @Codeflix-bots  
+#- ғᴏʀ ᴀɴʏ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ ᴍᴇ -> ᴛᴇʟᴇɢʀᴀᴍ @codeflix_bots Community @Otakflix_Network </b>
